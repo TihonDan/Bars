6dcb80994f9802fe9b80a94412a48e657890d2d7
Danila.T <roman-petrov-05@inbox.ru>
2022-03-18 16:12:02 +0300

    Изменил

--- Bars/Program.cs
+++ Bars/Program.cs
@@ -6,7 +6,7 @@ namespace Bars
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Bars!");//Welcome
         }
     }
 }
